b9535e70fe5ffaa48752526a9cca9e504aa44488
vldr <reinisvlad@gmail.com>
2023-12-20 20:52:37 -0600

    Convert to fixed width integer types.

--- src/Network.h
+++ src/Network.h
@@ -11,10 +11,10 @@ class Game;
 class Network
 {
 public:
-	void init(Game* game);
+    void init(Game* game);
     void connect();
-	void tick();
-	void render();
+    void tick();
+    void render();
 
     void sendPosition(const glm::vec3& position, const glm::vec2& rotation);
     void sendLevel(unsigned char index, bool respawn);
@@ -22,7 +22,7 @@ public:
 
     void join(const std::string& id);
     void create();
-    
+
     bool isConnected();
     bool isHost();
     size_t count();
@@ -34,29 +34,35 @@ public:
 
     std::string url;
 private:
-	void send(const std::string& text);
-	void sendBinary(unsigned char* data, size_t size);
+    void send(const std::string& text);
+    void sendBinary(unsigned char* data, size_t size);
 
     const char* BASE_URL = "https://cubic.vldr.org/#";
 
 #if defined(EMSCRIPTEN)
-	const char* URI = "wss://relay.vldr.org/";
+    const char* URI = "wss://relay.vldr.org/";
 #else
-	const char* URI = "ws://vldr.org:1234";
+    const char* URI = "ws://vldr.org:1234";
 #endif
 
-    enum class PacketType : unsigned char
+#pragma pack(push, 1)
+    enum class PacketType : uint8_t
     {
         Level,
         Position,
         SetBlock,
     };
 
+    static_assert(alignof(PacketType) == 1 && sizeof(PacketType) == 1);
+#pragma pack(pop)
+
 #pragma pack(push, 1)
     struct Packet
     {
-        unsigned char index;
+        uint8_t index;
     };
+
+    static_assert(alignof(Packet) == 1 && sizeof(Packet) == 1);
 #pragma pack(pop)
 
 #pragma pack(push, 1)
@@ -64,11 +70,13 @@ private:
     {
         PacketType type = PacketType::Level;
 
-        bool respawn;
+        uint8_t respawn;
 
-        int length;
-        unsigned char data[2 * Level::WIDTH * Level::HEIGHT * Level::DEPTH];
+        uint32_t length;
+        uint8_t data[2 * Level::WIDTH * Level::HEIGHT * Level::DEPTH];
     };
+
+    static_assert(alignof(LevelPacket) == 1 && sizeof(LevelPacket) == 7 + 2 * Level::WIDTH * Level::HEIGHT * Level::DEPTH);
 #pragma pack(pop)
 
 #pragma pack(push, 1)
@@ -76,9 +84,11 @@ private:
     {
         PacketType type = PacketType::Position;
 
-        glm::vec3 position;
-        glm::vec2 rotation;
+        glm::f32vec3 position;
+        glm::f32vec2 rotation;
     };
+
+    static_assert(alignof(PositionPacket) == 1 && sizeof(PositionPacket) == 22);
 #pragma pack(pop)
 
 #pragma pack(push, 1)
@@ -86,16 +96,19 @@ private:
     {
         PacketType type = PacketType::SetBlock;
 
-        glm::ivec3 position;
-        unsigned char blockType;
-        bool mode;
-    };
-#pragma pack(pop)
+        glm::i32vec3 position;
 
-	std::vector<std::unique_ptr<Player>> players;
-    std::vector<PositionPacket> positionPackets;
+        uint8_t blockType;
+        uint8_t mode;
+    };
+
+    static_assert(alignof(SetBlockPacket) == 1 && sizeof(SetBlockPacket) == 16);
+#pragma pack(pop)
 
     bool connected;
 
-	Game* game;
+    std::vector<std::unique_ptr<Player>> players;
+    std::vector<PositionPacket> positionPackets;
+
+    Game* game;
 };
