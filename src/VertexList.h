bffee3378e8ab7fa9eb775e49d13bd3c0db81765
Vlad <vlad@vldr.org>
2024-10-21 21:16:57 -0500

    Simplify vertex list constructor.

--- src/VertexList.h
+++ src/VertexList.h
@@ -8,101 +8,9 @@ class VertexList
 public:
   struct Vertex
   {
-    inline Vertex(float x, float y, float z, float u, float v, float s)
-      : x(x), y(y), z(z), u(u), v(v), s(s) {}
-
-    inline Vertex(int x, float y, float z, float u, float v, float s)
-      : x((float)x), y(y), z(z), u(u), v(v), s(s) {}
-
-    inline Vertex(int x, int y, float z, float u, float v, float s)
-      : x((float)x), y((float)y), z(z), u(u), v(v), s(s) {}
-
-    inline Vertex(int x, int y, int z, float u, float v, float s)
-      : x((float)x), y((float)y), z((float)z), u(u), v(v), s(s) {}
-
-    inline Vertex(float x, int y, int z, float u, float v, float s)
-      : x(x), y((float)y), z((float)z), u(u), v(v), s(s) {}
-
-    inline Vertex(float x, int y, float z, float u, float v, float s)
-      : x(x), y((float)y), z(z), u(u), v(v), s(s) {}
-
-    inline Vertex(float x, float y, int z, float u, float v, float s)
-      : x(x), y(y), z((float)z), u(u), v(v), s(s) {}
-
-    inline Vertex(int x, float y, int z, float u, float v, float s)
-      : x((float)x), y(y), z((float)z), u(u), v(v), s(s) {}
-
-    inline Vertex(float x, float y, float z, int u, float v, float s)
-      : x(x), y(y), z(z), u((float)u), v(v), s(s) {}
-
-    inline Vertex(int x, float y, float z, int u, float v, float s)
-      : x((float)x), y(y), z(z), u((float)u), v(v), s(s) {}
-
-    inline Vertex(int x, int y, float z, int u, float v, float s)
-      : x((float)x), y((float)y), z(z), u((float)u), v(v), s(s) {}
-
-    inline Vertex(int x, int y, int z, int u, float v, float s)
-      : x((float)x), y((float)y), z((float)z), u((float)u), v(v), s(s) {}
-
-    inline Vertex(float x, int y, int z, int u, float v, float s)
-      : x(x), y((float)y), z((float)z), u((float)u), v(v), s(s) {}
-
-    inline Vertex(float x, int y, float z, int u, float v, float s)
-      : x(x), y((float)y), z(z), u((float)u), v(v), s(s) {}
-
-    inline Vertex(float x, float y, int z, int u, float v, float s)
-      : x(x), y(y), z((float)z), u((float)u), v(v), s(s) {}
-
-    inline Vertex(int x, float y, int z, int u, float v, float s)
-      : x((float)x), y(y), z((float)z), u((float)u), v(v), s(s) {}
-    
-    inline Vertex(float x, float y, float z, float u, int v, float s)
-      : x(x), y(y), z(z), u(u), v((float)v), s(s) {}
-
-    inline Vertex(int x, float y, float z, float u, int v, float s)
-      : x((float)x), y(y), z(z), u(u), v((float)v), s(s) {}
-
-    inline Vertex(int x, int y, float z, float u, int v, float s)
-      : x((float)x), y((float)y), z(z), u(u), v((float)v), s(s) {}
-
-    inline Vertex(int x, int y, int z, float u, int v, float s)
-      : x((float)x), y((float)y), z((float)z), u(u), v((float)v), s(s) {}
-
-    inline Vertex(float x, int y, int z, float u, int v, float s)
-      : x(x), y((float)y), z((float)z), u(u), v((float)v), s(s) {}
-
-    inline Vertex(float x, int y, float z, float u, int v, float s)
-      : x(x), y((float)y), z(z), u(u), v((float)v), s(s) {}
-
-    inline Vertex(float x, float y, int z, float u, int v, float s)
-      : x(x), y(y), z((float)z), u(u), v((float)v), s(s) {}
-
-    inline Vertex(int x, float y, int z, float u, int v, float s)
-      : x((float)x), y(y), z((float)z), u(u), v((float)v), s(s) {}
-
-    inline Vertex(float x, float y, float z, int u, int v, float s)
-      : x(x), y(y), z(z), u((float)u), v((float)v), s(s) {}
-
-    inline Vertex(int x, float y, float z, int u, int v, float s)
-      : x((float)x), y(y), z(z), u((float)u), v((float)v), s(s) {}
-
-    inline Vertex(int x, int y, float z, int u, int v, float s)
-      : x((float)x), y((float)y), z(z), u((float)u), v((float)v), s(s) {}
-
-    inline Vertex(int x, int y, int z, int u, int v, float s)
-      : x((float)x), y((float)y), z((float)z), u((float)u), v((float)v), s(s) {}
-
-    inline Vertex(float x, int y, int z, int u, int v, float s)
-      : x(x), y((float)y), z((float)z), u((float)u), v((float)v), s(s) {}
-
-    inline Vertex(float x, int y, float z, int u, int v, float s)
-      : x(x), y((float)y), z(z), u((float)u), v((float)v), s(s) {}
-
-    inline Vertex(float x, float y, int z, int u, int v, float s)
-      : x(x), y(y), z((float)z), u((float)u), v((float)v), s(s) {}
-
-    inline Vertex(int x, float y, int z, int u, int v, float s)
-      : x((float)x), y(y), z((float)z), u((float)u), v((float)v), s(s) {}
+    template<typename T1, typename T2, typename T3, typename T4, typename T5, typename T6>
+    inline Vertex(T1 x, T2 y, T3 z, T4 u, T5 v, T6 s)
+      : x((float)x), y((float)y), z((float)z), u((float)u), v((float)v), s((float)s) {}
 
     float x;
     float y;
@@ -114,8 +22,7 @@ public:
 
   struct Allocator
   {
-    Allocator(size_t size = 4) 
-      : size(size)
+    Allocator(size_t size = 4) : size(size)
     {
       data = static_cast<Vertex*>(std::malloc(size * sizeof(*data)));
     }
@@ -152,6 +59,7 @@ public:
         std::realloc(allocator->data, allocator->size * sizeof(VertexList::Vertex))
       );
     }
+
     allocator->data[index++] = VertexList::Vertex(std::forward<Args>(args)...);
   }
 
